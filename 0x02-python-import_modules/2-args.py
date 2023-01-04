a1ab361b6dc42827917e7618835ab05f2543ef5e
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-01-04 11:38:13 +0100

    removed 's' from arguments of the elif\

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,12 @@
 #!/usr/bin/python3
-# 2-args.py
-# Brennan D Baraban <375@holbertonschool.com>
-
 if __name__ == "__main__":
-    """Print the number of and list of arguments."""
     import sys
-
-    count = len(sys.argv) - 1
-    if count == 0:
-        print("0 arguments.")
-    elif count == 1:
-        print("1 argument:")
-    else:
-        print("{} arguments:".format(count))
-    for i in range(count):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]))
+    num = len(sys.argv) - 1
+if num == 0:
+    print("{:d} {:s}{:s}".format(num, "arguments", "."))
+elif num == 1:
+    print("{:d} {:s}{:s}".format(num, "argument", ":"))
+else:
+    print("{:d} arguments:".format(num))
+for i in range(num):
+    print("{:d}: {:s}".format(i + 1, sys.argv[i + 1]))
