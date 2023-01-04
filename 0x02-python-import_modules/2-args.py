1acbf3321d25ccf619926fb2d3943bbb0607fc3f
vincentcheri <vj9783@gmail.com>
2023-01-04 01:59:01 -0800

    make your script dynamic

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    num = len(sys.argv) - 1
+if num == 0:
+    print("{:d} {:s}{:s}".format(num, "arguments", "."))
+elif num == 1:
+    print("{:d} {:s}{:s}".format(num, "arguments", ":"))
+else:
+    print("{} arguments:".format(num))
+for i in range(num):
+    print("{}: {}".format(i + 1, sys.argv[i + 1]))
