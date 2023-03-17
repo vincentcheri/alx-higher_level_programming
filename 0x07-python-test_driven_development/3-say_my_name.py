571dcf0b14197a7babdcc0bb401cb4b9685cb1cb
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-03-17 19:19:39 +0100

    python standerd documentation

--- 0x07-python-test_driven_development/3-say_my_name.py
+++ 0x07-python-test_driven_development/3-say_my_name.py
@@ -1,20 +1,19 @@
 #!/usr/bin/python3
-"""Module containin function to print name.
-To be used as a module. Contains no directly executable code.
-"""
+# 3-say_my_name.py
+
+"""Defines a name-printing function."""
 
 
 def say_my_name(first_name, last_name=""):
-    """Function that prints a name
+    """Print a name.
     Args:
-        first_name (str): First name to print
-        last_name (str): Last name to print
-    Returns: None
+        first_name (str): The first name to print.
+        last_name (str): The last name to print.
     Raises:
-        TypeError: If `first_name` or `last_name` are not strings.
+        TypeError: If either of first_name or last_name are not strings.
     """
-    if type(first_name) is not str:
-        raise TypeError('first_name must be a string')
-    if type(last_name) is not str:
-        raise TypeError('last_name must be a string')
-    print("My name is {:s} {:s}".format(first_name, last_name))
+    if not isinstance(first_name, str):
+        raise TypeError("first_name must be a string")
+    if not isinstance(last_name, str):
+        raise TypeError("last_name must be a string")
+    print("My name is {} {}".format(first_name, last_name))
