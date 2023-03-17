1b8d03241797f5acecc24e47440e4f81b2000385
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-03-17 19:20:48 +0100

    print square

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-"""Module containing function to print a square
-Should not be executed directly, but only imported as a module
-"""
+# 4-print_square.py
+
+"""Defines a square-printing function."""
 
 
 def print_square(size):
-    """Function to print a square with sides `size`.
+    """Print a square with the # character.
     Args:
-        size (int): Integral side length of square
-    Returns: None
+        size (int): The height/width of the square.
     Raises:
-        TypeError: If `size` is not an integer.
-        ValueError: If `size` is less than 0.
+        TypeError: If size is not an integer.
+        ValueError: If size is < 0
     """
-    if type(size) is not float and type(size) is not int:
-        raise TypeError('size must be an integer')
-    if type(size) is float and size != round(size):
-        raise TypeError('size must be an integer')
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
     if size < 0:
-        raise ValueError('size must be >= 0')
+        raise ValueError("size must be >= 0")
 
-    for i in range(round(size)):
-        print("#" * round(size))
+    for i in range(size):
+        [print("#", end="") for j in range(size)]
+        print("")
