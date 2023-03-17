5635c9108a41a30901ec45da7a5e7b96ac069edc
vincentcheri <vj9783@gmail.com>
2023-03-17 04:18:56 -0700

    Test driven development

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+"""Module containing function to divide 2d matrix elementwise.
+Not directly executable, but can be imported.
+"""
+
+
+def matrix_divided(matrix, div):
+    """Function to divide all elements of `matrix` by `div`
+    Args:
+        matrix: 2d array (list of lists) of integers/floats.
+        div: Divisor of `matrix`. Must be either integer or float.
+    Returns:
+        New matrix with result of the division of each element of
+        `matrix` by `div`.
+    Raises:
+        TypeError: If `matrix` not 2d or contains non-int/float
+            elements or `div` is neither an integer nor float.
+            Also if `matrix` is ragged (not all rows of equal length).
+        ZeroDivisionError: If `div` is 0.
+        OverflowError: If `matrix` contains '+/-inf' or 'nan'.
+    """
+    if not isinstance(div, (float, int)) or div != div:
+        raise TypeError('div must be a number')
+    if div == 0:
+        raise ZeroDivisionError('division by zero')
+    if type(matrix) is not list:
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    types_row = map(lambda x: isinstance(x, list), matrix)
+    if not all(types_row):
+        raise TypeError('matrix must be a matrix (list of lists) '
+                        'of integers/floats')
+    for row in matrix:
+        types_element = map(lambda x: isinstance(x, (float, int)), row)
+        if not all(types_element):
+            raise TypeError('matrix must be a matrix (list of lists) '
+                            'of integers/floats')
+    for row in matrix:
+        types_element = map(lambda x: x == x
+                            and x != float('inf')
+                            and x != -float('inf'),
+                            row)
+        if not all(types_element):
+            raise OverflowError
+    sizes = map(lambda x: len(x) == len(matrix[0]), matrix)
+    if not all(sizes):
+        raise TypeError('Each row of the matrix must have the same size')
+
+    mat_div = list(map(lambda x:
+                       list(map(lambda y:
+                                round(y / div, 2), x)), matrix))
+    return mat_div
---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Module containing function to print strings by line
+Contains no directly executable code. Should be imported
+as a module.
+"""
+
+
+def text_indentation(text):
+    """Function to print text double-spaced, using `.?:` as
+    line separators.
+    Args:
+        text (str): String to print
+    Returns: None.
+    Raises:
+        TypeError: If `text` not a string.
+    """
+    delims = '.:?'
+    if type(text) is not str:
+        raise TypeError('text must be a string')
+    for c in delims:
+        text = str(c + '\n\n').join(s.strip() for s in text.split(c))
+    print(text, end='')
+    if len(text) > 0 and text[-1] in delims:
+        print('\n')
