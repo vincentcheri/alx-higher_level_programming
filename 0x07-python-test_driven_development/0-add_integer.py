e500dbc27b12c082ec18f7ee7b6d362d3f9ec7d0
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-03-17 18:43:21 +0100

    adjusted the documentation

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,29 +1,17 @@
 #!/usr/bin/python3
-"""Module containing simple function to add together two integers
-Must import this module to use it. Cannot be executed directly.
-To run doctests:
-    $ python3 -m doctest -v ./tests/0-add_integer.txt
-"""
+# 0-add_integer.py
+
+"""Defines an integer addition function."""
 
 
-def add_integer(a, b):
-    """Add two integers
-    Args:
-        a: First integer to add. If type(a) is float, it is first
-            casted to an int.
-        b: Second integer to add. If type(b) is float, it is first
-            casted to an int.
-    Returns:
-        Sum of `a` and `b`
+def add_integer(a, b=98):
+    """Return the integer addition of a and b.
+    Float arguments are typecasted to ints before addition is performed.
     Raises:
-        TypeError: If `type(a)` or `type(b)` is neither an int nor
-        float.
-"""
-    if type(a) is not int and type(a) is not float:
-        raise TypeError('a must be an integer')
-    if type(b) is not int and type(b) is not float:
-        raise TypeError('b must be an integer')
-    result = a + b
-    if result != result or result == float('inf') or result == -float('inf'):
-        raise OverflowError
-    return int(a) + int(b)
+        TypeError: If either of a or b is a non-integer and non-float.
+    """
+    if ((not isinstance(a, int) and not isinstance(a, float))):
+        raise TypeError("a must be an integer")
+    if ((not isinstance(b, int) and not isinstance(b, float))):
+        raise TypeError("b must be an integer")
+    return (int(a) + int(b))
