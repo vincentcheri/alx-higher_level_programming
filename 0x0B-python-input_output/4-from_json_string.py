6bf653a6c89d8bb49398c5e7b074793059f3b980
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-03-20 11:31:47 +0100

    Update 4-from_json_string.py

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
+"""
+Json Module
+"""
 import json
-"""
-This file contains a function that
-returns an object represented by a
-JSON string
-"""
+
 
 def from_json_string(my_str):
+    """convert str to object
+    args:
+        my_str: string to convert
+    return:
+        object
     """
-    function to return object
-    represented by a JSON string
-    """
-    return (json.loads(my_str))
+
+    return json.loads(my_str)
