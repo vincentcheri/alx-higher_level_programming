ba221b58e9dbd3a0077e92f9a5975e5aa54ef328
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-03-20 12:05:28 +0100

    removed double colon

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-# 101-stats.py
-
-"""Reads from standard input and computes metrics.
+"""
+Read from standard input and computes metrics.
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.
 """
 
+
 def print_stats(size, status_codes):
-    """Print accumulated metrics.
+    """
+    Print accumulated metrics.
     Args:
         size (int): The accumulated read file size.
         status_codes (dict): The accumulated count of status codes.
@@ -17,6 +18,7 @@ def print_stats(size, status_codes):
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
