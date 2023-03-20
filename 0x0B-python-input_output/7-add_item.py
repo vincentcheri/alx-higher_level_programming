54957b5e141389f2bd8d66eb219b66e9c0a1d536
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-03-20 11:49:08 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,17 +1,15 @@
 #!/usr/bin/python3
-from sys import argv
-"""access commandline arguments"""
-load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
-"""create object from JSON file"""
-save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
-"""writes an object to text file, using JSON representation"""
+""" Add all arguments to a Python list and save them to a file """
+import sys
 
-filename = "add_item.json"
-try:
-    content = load_from_json_file(filename)
-except:
-    content = []
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
-for i in range(1, len(argv)):
-    content.append(argv[i])
-save_to_json_file(content, filename)
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
