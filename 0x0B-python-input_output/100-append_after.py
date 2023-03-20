76e7d8840260866ae65e1af0ed0bf439b81b3386
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-03-20 12:03:21 +0100

    fix excess lines

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,23 +1,26 @@
 #!/usr/bin/python3
 """
-This file contains a function
-that inserts a line of text to a file
-after each line containing a specific string
+Module that executes a function that inserts a line
+of text to a file, after each line containing a specific
+string.
 """
 
 
 def append_after(filename="", search_string="", new_string=""):
     """
-    function that inserts text
-    after a specific line
+    Append a new line when a string is found
+    Args:
+        filename: filename
+        search_string: string to search
+        new_string: string to append
     """
-    string = ""
-    with open(filename, encoding="utf-8") as myFile:
-        for line in myFile:
-            string += line
-            if search_string in line:
-                string += new_string
 
-    with open(filename, mode="w") as myFile:
-        myFile.write(string)
+    res_line = []
+    with open(filename, 'r', encoding="utf-8") as f:
+        for line in f:
+            res_line += [line]
+            if line.find(search_string) != -1:
+                res_line += [new_string]
 
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write("".join(res_line))
