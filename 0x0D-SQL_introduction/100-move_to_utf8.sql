28d6a1c69ef7860c74c387583936895fda8a6382
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-03-23 10:50:19 +0100

    Update 100-move_to_utf8.sql

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,16 @@
--- script that converts database to UTF-8
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+-- Convert `hbtn_0c_0` db and table `first_table`
+-- as well as column `name` to utfmb4 encoding.
 USE hbtn_0c_0;
-ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE
+      hbtn_0c_0
+      CHARACTER SET = utf8mb4
+      COLLATE = utf8mb4_unicode_ci;
+ALTER TABLE
+      first_table
+      CONVERT TO CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
+ALTER TABLE
+      first_table
+      CHANGE `name` `name` VARCHAR(256)
+      CHARACTER SET utf8mb4
+      COLLATE utf8mb4_unicode_ci;
