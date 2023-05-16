c73c8b410b0b59fef9d744b37f5bb4fe8818acc1
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-05-16 11:27:04 -0400

    add_me_maybe

--- 0x12-javascript-warm_up/102-add_me_maybe.js
+++ 0x12-javascript-warm_up/102-add_me_maybe.js
@@ -1,8 +1,5 @@
 #!/usr/bin/node
-// JS Script
-
-exports.callMeMoby = function (y, theFunction) {
-  for (let j = 0; j < y; j++) {
-    theFunction();
-  }
+exports.addMeMaybe = function (num, theFunction) {
+  num++;
+  theFunction(num);
 };
