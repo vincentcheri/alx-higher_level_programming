467df446bf8988c3db7035fd94eaeaf785325275
vincentcheri <vj9783@gmail.com>
2023-05-17 12:28:30 -0700

    sorted

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
 const newDict = {};
-for (let key in dict) {
+for (const key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [key];
   } else {
