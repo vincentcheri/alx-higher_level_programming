86751b1b4757cd1acb5e2248f7405a2aff84bf31
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-05-17 15:14:49 -0400

    Update 101-sorted.js

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const dict = require('./101-data.js').dict;
-let newDict = {};
+const newDict = {};
 for (let key in dict) {
   if (newDict[dict[key]] === undefined) {
     newDict[dict[key]] = [key];
