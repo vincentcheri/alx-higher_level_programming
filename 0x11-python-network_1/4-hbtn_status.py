7e0125687702d8d7e58b2ab4e692a936fb5823ac
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-06-19 10:06:45 -0400

    removed too much ':'

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+"""Fetches https://intranet.hbtn.io/status."""
 import requests
-"""
-script that fetches url only with requests
-"""
 
 
 if __name__ == "__main__":
-    reply = requests.get('https://intranet.hbtn.io/status').text
+    r = requests.get("https://intranet.hbtn.io/status")
     print("Body response:")
-    print("\t- type: {}".format(type(reply)))
-    print("\t- content: {}".format(reply))
+    print("\t- type: {}".format(type(r.text)))
+    print("\t- content: {}".format(r.text))
