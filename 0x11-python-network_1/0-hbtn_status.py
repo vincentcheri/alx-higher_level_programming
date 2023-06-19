e557d8d0494f772eff6350a431e2f9601e1bed5a
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-06-19 09:35:47 -0400

    0-hbtn_status.py

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import urllib.request
 """
-Script that fetches https://intranet.hbtn.io/status!!!!!
+Script that fetches https://alx-intranet.hbtn.io/status
 """
 
 
 if __name__ == "__main__":
-    with urllib.request.urlopen('https://intranet.hbtn.io/status') as reply:
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status
+
+') as reply:
         body = reply.read()
     print("Body response:")
     print("\t- type: {}".format(type(body)))
