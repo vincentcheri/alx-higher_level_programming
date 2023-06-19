3f030eddc2e0ef11a10e51538eba493aced25b25
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-06-19 10:29:30 -0400

    added utf-8 3-error_code.py

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -15,6 +15,6 @@ if __name__ == "__main__":
     request = urllib.request.Request(url)
     try:
         with urllib.request.urlopen(request) as response:
-            print(response.read().decode("ascii"))
+            print(response.read().decode("utf-8"))
     except urllib.error.HTTPError as e:
         print("Error code: {}".format(e.code))
