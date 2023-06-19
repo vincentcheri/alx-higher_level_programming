5095b9d426b035bf12a56c845776c804c85b681b
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-06-19 10:09:39 -0400

    7-error_code.py

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -1,15 +1,18 @@
 #!/usr/bin/python3
+"""Sends a request to a given URL and displays the response body.
+
+Usage: ./7-error_code.py <URL>
+  - Handles HTTP errors.
+"""
+import sys
 import requests
-from sys import argv
-"""
-script to send requests to url and display body of response
-"""
 
 
 if __name__ == "__main__":
-    reply = requests.get(argv[1])
-    code = reply.status_code
-    if code > 400:
-        print("Error code: {}".format(code))
+    url = sys.argv[1]
+
+    r = requests.get(url)
+    if r.status_code >= 400:
+        print("Error code: {}".format(r.status_code))
     else:
-        print(reply.text)
+        print(r.text)
