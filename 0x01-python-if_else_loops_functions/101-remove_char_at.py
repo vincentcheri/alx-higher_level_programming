dfc0ffa832a936979059819598f35200ad4abd4f
vincentcheri <52957917+vincentcheri@users.noreply.github.com>
2023-07-04 05:15:49 -0400

    This script removes a particular character from 'str'

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python3
+"""This script removes a particular character from 'str'. where n is the position of the char"""
+
 def remove_char_at(str, n):
     if n < 0:
         return (str)
